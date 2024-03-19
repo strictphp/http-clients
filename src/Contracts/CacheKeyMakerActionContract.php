4e9e65cc80780e4cd4e4806846d82de5b9f7dd7b
Milan Matějček <milan.matejcek@gmail.com>
2024-03-19 14:01:39 +0100

    feat(CacheKeyMakerAction): make identifier directly

--- src/Contracts/CacheKeyMakerActionContract.php
+++ src/Contracts/CacheKeyMakerActionContract.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace StrictPhp\HttpClients\Contracts;
 
-use Psr\Http\Message\StreamInterface;
-use Psr\Http\Message\UriInterface;
+use Psr\Http\Message\RequestInterface;
 
 interface CacheKeyMakerActionContract
 {
-    /**
-     * @param array<string, array<string>|string> $headers
-     */
-    public function execute(array &$headers, StreamInterface $body, UriInterface $uri): void;
+    public function execute(RequestInterface $request): string;
 }
