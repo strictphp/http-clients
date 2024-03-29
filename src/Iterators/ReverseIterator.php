866e26cdd2bd656ac0f6d33ac1ae3c7eb84dee84
Milan Matějček <milan.matejcek@gmail.com>
2024-03-29 03:52:45 +0100

    feat(ReverseIterator): better way how loop array reverse

---
+++ src/Iterators/ReverseIterator.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace StrictPhp\HttpClients\Iterators;
+
+use Generator;
+use IteratorAggregate;
+use StrictPhp\HttpClients\Contracts\ClientFactoryContract;
+
+/**
+ * @template T
+ * @implements IteratorAggregate<int, ClientFactoryContract>
+ */
+final class ReverseIterator implements IteratorAggregate
+{
+    /**
+     * @param array<T> $factories
+     */
+    public function __construct(private readonly array $factories)
+    {
+    }
+
+    /**
+     * @return Generator<T>
+     */
+    public function getIterator(): Generator
+    {
+        $count = count($this->factories) - 1;
+
+        for ($i = $count; $i >= 0; --$i) {
+            yield $this->factories[$i];
+        }
+    }
+
+}
