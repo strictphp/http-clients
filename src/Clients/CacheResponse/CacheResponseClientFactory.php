2f9f7e3d193c87f43874d40ff66eb00c7ab5672f
Milan Matějček <milan.matejcek@gmail.com>
2024-04-09 05:13:03 +0200

    feat(CacheResponseClientFactory): ConfigManager is required

--- src/Clients/CacheResponse/CacheResponseClientFactory.php
+++ src/Clients/CacheResponse/CacheResponseClientFactory.php
@@ -12,13 +12,13 @@ final class CacheResponseClientFactory implements ClientFactoryContract
 {
     public function __construct(
         private readonly CacheInterface $cache,
-        private readonly ?ConfigManager $configs = null,
+        private readonly ConfigManager $configManager,
     ) {
     }
 
     public function create(ClientInterface $client): ClientInterface
     {
-        return new CacheResponseClient($client, $this->cache, $this->configs ?? new ConfigManager());
+        return new CacheResponseClient($client, $this->cache, $this->configManager);
     }
 
 }
