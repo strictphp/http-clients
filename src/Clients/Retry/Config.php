e13c28cc7e66c3230589f104fefd15b24b5b15ca
Milan Matějček <milan.matejcek@gmail.com>
2024-04-11 08:52:11 +0200

    feat(Retry): add new client Retry

---
+++ src/Clients/Retry/Config.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace StrictPhp\HttpClients\Clients\Retry;
+
+use Psr\Http\Client\ClientExceptionInterface;
+use StrictPhp\HttpClients\Contracts\ConfigContract;
+use Throwable;
+
+final class Config implements ConfigContract
+{
+    /**
+     * @var callable(Throwable): bool
+     * @readonly
+     */
+    public $isMyException;
+
+    /**
+     * @param positive-int $tries
+     */
+    public function __construct(public readonly int $tries = 2, ?callable $isMyException = null)
+    {
+        $this->isMyException = $isMyException ?? static fn (Throwable $e): bool => $e instanceof ClientExceptionInterface;
+    }
+
+    public function initFromDefaultConfig(ConfigContract $object): void
+    {
+        // intentionally empty
+    }
+
+}
---
+++ src/Clients/Retry/RetryClientFactory.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace StrictPhp\HttpClients\Clients\Retry;
+
+use Psr\Http\Client\ClientInterface;
+use StrictPhp\HttpClients\Contracts\ClientFactoryContract;
+use StrictPhp\HttpClients\Managers\ConfigManager;
+
+final class RetryClientFactory implements ClientFactoryContract
+{
+    public function __construct(private readonly ConfigManager $configManager)
+    {
+    }
+
+    public function create(ClientInterface $client): ClientInterface
+    {
+        return new RetryClient($client, $this->configManager);
+    }
+
+}
