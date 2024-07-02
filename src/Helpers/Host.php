008924e9255dbf39c12464ed795c8b334b973f16
Milan Matějček <milan.matejcek@gmail.com>
2024-07-02 08:15:10 +0200

    test(Host)

--- src/Helpers/Host.php
+++ src/Helpers/Host.php
@@ -2,19 +2,30 @@
 
 namespace StrictPhp\HttpClients\Helpers;
 
+use StrictPhp\HttpClients\Exceptions\InvalidStateException;
+
 final class Host
 {
     /**
-     * @var array<string, string>
+     * @var array<non-empty-string, non-empty-string>
      */
     private static array $hosts = [];
 
+    /**
+     * @param non-empty-string $host
+     * @return non-empty-string
+     */
     public static function allSubdomains(string $host): string
     {
         if (array_key_exists($host, self::$hosts)) {
             return self::$hosts[$host];
         }
 
-        return self::$hosts[$host] = '*.' . implode('.', array_slice(explode('.', $host), -2));
+        $levels = explode('.', $host);
+        if (count($levels) < 2) {
+            throw new InvalidStateException(sprintf('Minimum levels for domain is 2. Not allowed: %s', $host));
+        }
+
+        return self::$hosts[$host] = '*.' . implode('.', array_slice($levels, -2));
     }
 }
